7f3d9988f166ad8815fde8c7b61b98c95d37b1a6
Vinicius Carvalho <viniciuscdess04@gmail.com>
2025-09-30 12:41:54 -0300

    routes e controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Hello API!']);
-});
+// Route::get('/hello', function () {
+//     return response()->json(['message' => 'Hello API!']);
+// });
+
+Route::apiResource('clients', ClientController::class);
+
+// Route::get('clients', [ClientController::class, 'index']);
